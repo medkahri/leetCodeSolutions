1010fc1bec57c0eb7411502e4d9701b300814afe
medkahri <75744701+medkahri@users.noreply.github.com>
2022-09-13 22:22:29 +0100

    Add files via upload

---
+++ twoSum.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        int[] res ;
+        res = new int[2];
+
+        for (int i = 0; i < nums.length; i++){
+
+            for (int j = 0; j < nums.length; j++){
+
+                if ((nums[i]+nums[j] == target) && (i!=j)){
+
+                    res[0] = j;
+                    res[1] = i ;
+
+                }
+            }
+
+
+
+
+
+
+        }
+
+
+        return res;
+
+
+    }
+}
